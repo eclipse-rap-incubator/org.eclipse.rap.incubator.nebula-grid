fabf73a44cc31ee753adebea19ecaff8191f0488
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-06-14 10:13:14 +0300

    Run as OSGI application

---
+++ bundles/org.eclipse.rap.nebula.widgets.grid.snippets/src/org/eclipse/rap/nebula/widgets/grid/snippets/internal/GridSnippetsApplication.java
@@ -0,0 +1,22 @@
+package org.eclipse.rap.nebula.widgets.grid.snippets.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.nebula.widgets.grid.snippets.GridSnippet1;
+import org.eclipse.rwt.application.Application;
+import org.eclipse.rwt.application.Application.OperationMode;
+import org.eclipse.rwt.application.ApplicationConfiguration;
+import org.eclipse.rwt.client.WebClient;
+
+
+public class GridSnippetsApplication implements ApplicationConfiguration {
+
+  public void configure( Application application ) {
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put( WebClient.PAGE_TITLE, "Nebula Grid Snippets" );
+    application.addEntryPoint( "/snippet1", GridSnippet1.class, properties );
+    application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
+  }
+
+}
