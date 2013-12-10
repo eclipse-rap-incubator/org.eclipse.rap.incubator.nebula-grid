8f0c85da1d76a8282d840a15c3220cbf1fa02999
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-12-10 14:40:47 +0100

    Make theme adapter non-final
    This allows third-party client implementations to experiment with
    sub-classed theme adapters. We consider final classes an obsolete
    practice as this also prevents mocking objects in tests.

--- bundles/org.eclipse.rap.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/internal/gridkit/GridThemeAdapter.java
+++ bundles/org.eclipse.rap.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/internal/gridkit/GridThemeAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 EclipseSource and others.
+ * Copyright (c) 2012, 2013 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,15 @@
  ******************************************************************************/
 package org.eclipse.nebula.widgets.grid.internal.gridkit;
 
-import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.internal.widgets.controlkit.ControlThemeAdapterImpl;
 import org.eclipse.swt.widgets.Control;
 
 
 @SuppressWarnings("restriction")
-public final class GridThemeAdapter extends ControlThemeAdapterImpl {
+public class GridThemeAdapter extends ControlThemeAdapterImpl {
 
   @Override
   public Font getFont( Control control ) {
